89dcbc8a0242180da37e581d2fd5b7d804b9902a
HakeemYB <120651914+HakeemYB@users.noreply.github.com>
2023-03-14 07:25:43 +0500

    Update node.tf

--- node.tf
+++ node.tf
@@ -34,8 +34,8 @@ resource "aws_eks_node_group" "private-nodes" {
   node_role_arn   = aws_iam_role.nodes.arn
 
   subnet_ids = [
-    aws_subnet.private-us-east-2a.id,
-    aws_subnet.private-us-east-2b.id
+    aws_subnet.private-us-east-1a.id,
+    aws_subnet.private-us-east-1b.id
   ]
 
 
